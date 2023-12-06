bd9a5e3d067ad6b654e6a2104417cda9859906e9
mechanburg <tubbl90@gmail.com>
2023-12-06 09:46:49 -0500

    new functions

--- filefunctions.py
+++ filefunctions.py
@@ -3,6 +3,7 @@ from zipfile import ZipFile, Path
 import pandas as pd
 import numpy as np
 from datetime import datetime, date
+import re
 
 
 
@@ -47,9 +48,8 @@ def log(zipname, levels):
                                         log_info["version"] = term.strip('v')
                                     else:
                                         log_info["version"] = term.strip('v')
-                                        # print(message[0][0:23])
                                         log_info["date updated"] = datetime.fromisoformat(message[0][0:23])
-                                        # print(log_info["date updated"])
+
                 if "Liberty instrument Connected" in decoded_line:
                     message = decoded_line.split('|')
                     for desc in message:
@@ -57,7 +57,7 @@ def log(zipname, levels):
                             message_items = desc.split(' ')
                             for term in message_items:
                                 if term.startswith('Serial'):
-                                    tempserial = term.split(":")[1].strip()#term.strip('SerialNumber:')#.strip()
+                                    tempserial = term.split(":")[1].strip()
                                     if tempserial == "LB0002":
                                         continue
                                     elif tempserial == log_info["serial"]:
@@ -70,7 +70,7 @@ def log(zipname, levels):
         return log_info
 
 
-def errorlog(zipname, levels):
+def errorlog(zipname, levels, serial):
     errors = pd.DataFrame()
     if 'Errors.txt' not in levels["level3"]:
         print("WARNING: No error log found")
@@ -90,16 +90,95 @@ def errorlog(zipname, levels):
                             message[3] = message[3].split('"')[1]
                             errors = pd.concat([errors, pd.Series([message[0], message[3]])], ignore_index = 1, axis = 1)
     errors = errors.transpose()
+    errors["serial"] = serial
+    errors.columns = ['Timestamp', "Description", "Serial"]
+    # print (type(errors.Timestamp.iloc[1]))
     return errors
 
-def errorcontext(zipname, errors, idx):
+def errorcontext(zipname, errors, idx, lookback = 15, lookforward = 5):
+    logtext = ""
     errortime = datetime.fromisoformat(errors.iloc[idx, 0][0:23])
-    errorfolder = str(errortime.year) + "-" + str(errortime.month)
-    print(type(errortime))
-    path = "/Logs/" + errorfolder + "/Run/"
+
+    year = str(errortime.year)
+    if len(str(errortime.month)) < 2:
+        month = "0" + str(errortime.month)
+    else:
+        month = str(errortime.month)
+    errorfolder = year + "-" + month
+
+    path = "Logs/" + errorfolder + "/Run/Run"
     with ZipFile(zipname) as zip:
-            df = pd.DataFrame(
-            [(zinfo.filename, zinfo.date_time, zinfo.file_size) for zinfo in zip.filelist],
-            columns=["filename", "date_time", "file_size"],)
-            print(type(df["date_time"].sort_values().iloc[3]))
-            print(df["date_time"].iloc[3]>errortime)
+        df = pd.DataFrame(
+        [(zinfo.filename, datetime(*zinfo.date_time), zinfo.file_size) for zinfo in zip.filelist],
+        columns=["filename", "date_time", "file_size"],)
+        # print(df)
+        runlogfiles = df[df.filename.str.contains(path)] #filter out only "Rundetail_XX.txt" files
+        errorfile = runlogfiles[runlogfiles.date_time >=errortime].sort_values(by="date_time").iloc[0,0] #remove the files that were updated prior to the datetime of the error in question
+        # print("Run Log:", errorfile)
+        notfound = True
+        errorline = {"timestamp":datetime(1990,1,1), "line number":0}
+        errordumpline = {"timestamp":datetime(1990,1,1), "line number":0}
+        searchfiles = [errorfile, path + "Detail_0.txt"]
+        for item in searchfiles:
+            linefile = zip.open(item).readlines()
+
+            for pos, line in enumerate(linefile):
+                try:
+                    decoded_line = line.decode(encoding="utf-8")
+                    if "|" not in decoded_line:
+                        # if "RegulatorFirmwareVersionNumber" in decoded_line:
+                        #     errordumpline = pos
+                        continue
+                    else:
+                        # try:
+                        timestamp = datetime.fromisoformat(decoded_line.split("|")[0][0:23])
+                        if "FirmwareVersionNumber:" in decoded_line:
+                            errordumpline["timestamp"] = timestamp
+                            errordumpline["line number"] = pos
+                        elif (((timestamp - errortime).seconds) < 1) & (((timestamp - errortime).days) >= 0):
+                            print("SEARCHED ERROR:",re.sub("[\t ]{2,}", " ", decoded_line))
+                            errorline["timestamp"] = timestamp
+                            errorline["line number"] = pos
+                            notfound = False
+                            # print(item,"\n")
+                            break
+                        # except:
+                        #     continue
+                except:
+                    continue #added due to error trying to decode "0xb0" byte in some random RunDetail log
+            if (notfound == False):
+                    break
+        if ((errorline["line number"] > 0) & ((errorline["line number"]-errordumpline["line number"]) > 0) & (((errorline["timestamp"] - errordumpline["timestamp"]).seconds) < 5)):
+            for line in linefile[errordumpline["line number"]-lookback:errorline["line number"]+1]:
+                line = re.sub("[\t ]{2,}", " ", line.decode(encoding="utf-8")).replace('\n', '')
+                logtext = logtext + "\n" + line
+                # logtext = logtext + line
+                # print(logtext)
+                # print(line.decode(encoding="utf-8").replace('\t', '').replace('\n', ''))
+        else:
+            for line in linefile[errorline["line number"]-lookback:errorline["line number"]+lookforward]:
+                line = re.sub("[\t ]{2,}", " ", line.decode(encoding="utf-8")).replace('\n', '')
+                logtext = logtext + "\n" + line
+                # logtext = logtext + line
+        # print(logtext)
+                # print(line.decode(encoding="utf-8").replace('\t', '').replace('\n', ''))
+        return logtext
+        # for line in zip.open(path + "Detail_0.txt"):
+        #     decoded_line = line.decode(encoding="utf-8")
+        #     if "|" not in decoded_line:
+        #         continue
+        #     else:
+        #         # print(decoded_line)
+        #         try:
+        #             timestamp = datetime.fromisoformat(decoded_line.split("|")[0][0:23])
+        #             if ((timestamp - errortime).seconds) < 1:
+        #                 print(decoded_line)
+        #                 print("Error found in RunDetail 0")
+        #                 notfound = False
+        #                 break
+        #         except:
+        #             continue
+        if (notfound):
+            print("WARNING: Error not found!")
+
+        # print(df["date_time"].iloc[20]>errortime)
--- readzip.py
+++ readzip.py
@@ -2,11 +2,12 @@ from filefunctions import *
 import tkinter
 from tkinter import filedialog
 import os
+import streamlit as st
 
 
 zipname = "Liberty PRIME 2.0 Troubleshooting Bundle - 2023-11-15 13.23.27.zip"
-zipname = "Liberty PRIME 2.0 Troubleshooting Bundle - 2023-09-18 10.05.56.zip"
-zipname = "Liberty PRIME 2.0 Troubleshooting Bundle - 2023-01-13 09.26.42.zip"
+# zipname = "Liberty PRIME 2.0 Troubleshooting Bundle - 2023-09-18 10.05.56.zip"
+# zipname = "Liberty PRIME 2.0 Troubleshooting Bundle - 2023-01-13 09.26.42.zip"
 # zipname = "C:/Users/lt259/Desktop/Liberty PRIME 2.0 Troubleshooting Bundle - 2023-11-21 10.32.48.zip"
 
 
@@ -25,11 +26,34 @@ root.withdraw() #use to hide tkinter window
 levels = findlevels(zipname)
 # print(levels)
 
-errors = errorlog(zipname, levels)
-print(errors)
-
-errorcontext(zipname, errors, 3)
-
 log_info = dict()
 log_info = log(zipname, levels)
-print(log_info)
+# print(log_info)
+
+errors = errorlog(zipname, levels, log_info["serial"])
+# print(errors)
+st.write(errors)
+
+option = st.selectbox("Pick an error to investigate:", errors["Timestamp"] + " | " + errors["Description"])
+# optiontime = datetime.fromisoformat(option.split("|")[0][0:23])
+# optiontime = pd.to_datetime(option.split("|")[0][0:23])
+optiontime = datetime.fromisoformat(option.split("|")[0][0:23])
+error_location = errors.set_index(pd.to_datetime(errors['Timestamp'])).index.get_indexer([optiontime], method='nearest')[0]
+print(errors.set_index(pd.to_datetime(errors['Timestamp'])).index.get_indexer([optiontime], method='nearest')[0])
+# iloc_idx = errors_timestamp.index.get_indexer([optiontime], method='nearest')
+
+# print (type(errors.Timestamp.iloc[1]))
+
+st.write(errorcontext(zipname, errors, error_location))
+# os.system("streamlit run readzip.py")
+
+# errorcontext(zipname, errors, 6)
+
+
+# for i in range(1,len(errors)):
+#
+#     errorcontext(zipname, errors, i)
+#     print("ERROR NUMBER:", i)
+#     print(errors[i-1:i+2])
+#
+#     k = input("NEXXXXXT:")
